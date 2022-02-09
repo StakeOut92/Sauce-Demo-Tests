edc84125ba26b02381da641393ba13c4541dbb5d
StakeOut92 <inozemtcew@gmail.com>
2022-02-09 18:50:36 +0300

    Added test to FinLocators class

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -30,8 +30,4 @@ public class BaseTest {
     public void shutOff() {
         driver.quit();
     }
-
-
-
-
 }
--- src/test/java/tests/SauceDemoTest.java
+++ src/test/java/tests/SauceDemoTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
